d375702b527da406eda56b3ee02f0e9bc60529b7
Jimmy Chen <jc219570512@gmail.com>
2025-01-12 16:34:09 -0500

    finish lecture 2 exercise

---
+++ LectureExercises/Lecture2/dimension_calc.py
@@ -0,0 +1,9 @@
+length = 16.5
+width = 12.5
+height = 5
+
+volume = length * width * height
+print("volume =", volume)
+
+area = 2 * ((length * width) + (length * height) + (width * height))
+print("area =", area)
