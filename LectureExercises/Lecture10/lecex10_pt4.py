1236adae185d785a8c4aa025cd279451f41113a6
c-jimmy1 <74884278+c-jimmy1@users.noreply.github.com>
2025-02-10 21:33:36 -0500

    bug fixes on lec10 pt 4

--- LectureExercises/Lecture10/lecex10_pt4.py
+++ LectureExercises/Lecture10/lecex10_pt4.py
@@ -1,14 +1,14 @@
 def increase_by_p(co2_levels, p):
     for i in range(len(co2_levels)):
-        co2_levels[i] = co2_levels[i] * (1 + p / 100)
+        co2_levels[i] = co2_levels[i] * (1 + p)
 
 co2_levels = [ 320.03, 322.16, 328.07, 333.91, 341.47, \
                348.92, 357.29, 363.77, 371.51, 382.47, 392.95 ]
 
-fraction = input('Enter the fraction: ').strip()
-print(fraction)
-float(fraction)
-increase_by_p(co2_levels, 10)
+p = input('Enter the fraction: ').strip()
+print(p)
+p = float(p)
+increase_by_p(co2_levels, p)
 
 print('First: {:.2f}'.format(co2_levels[0]))
 print('Last: {:.2f}'.format(co2_levels[-1]))
