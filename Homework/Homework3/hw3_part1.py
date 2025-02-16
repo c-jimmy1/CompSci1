5c0d459571b6e25d8fd2cc568b1c602467ee2b24
c-jimmy1 <74884278+c-jimmy1@users.noreply.github.com>
2025-02-16 13:51:43 -0500

    add guards/init part 2

--- Homework/Homework3/hw3_part1.py
+++ Homework/Homework3/hw3_part1.py
@@ -7,49 +7,49 @@ Version: 1
 """
 from syllables import find_num_syllables
 
+if __name__ == "__main__":
+    paragraph = input("Enter a paragraph => ").strip()
+    print(paragraph, '\n')
 
-paragraph = input("Enter a paragraph => ").strip()
-print(paragraph, '\n')
+    # Split the paragraph into words and count the number of periods in the paragraph.
+    words = paragraph.split()
+    periods = paragraph.count('.')
 
-# Split the paragraph into words and count the number of periods in the paragraph.
-words = paragraph.split()
-periods = paragraph.count('.')
-
-'''ASL = Average Sentence Length'''
-# Calculate the average sentence length of the paragraph. as length of words divided by number of periods.
-ASL = len(words)/periods
+    '''ASL = Average Sentence Length'''
+    # Calculate the average sentence length of the paragraph. as length of words divided by number of periods.
+    ASL = len(words)/periods
 
 
-'''PHW = Percentage of Hard Words'''
-# Count the number of hard words in the paragraph. Hard words are words with more than 3 syllables.
-hard_words = []
-for word in words:
-    if find_num_syllables(word) >= 3 and word.count('-') == 0:
-        if not word.endswith('es') and not word.endswith('ed'):
-            hard_words.append(word)
+    '''PHW = Percentage of Hard Words'''
+    # Count the number of hard words in the paragraph. Hard words are words with more than 3 syllables.
+    hard_words = []
+    for word in words:
+        if find_num_syllables(word) >= 3 and word.count('-') == 0:
+            if not word.endswith('es') and not word.endswith('ed'):
+                hard_words.append(word)
 
-# Calculate the percentage of hard words in the paragraph.
-PHW = len(hard_words)/len(words) * 100
+    # Calculate the percentage of hard words in the paragraph.
+    PHW = len(hard_words)/len(words) * 100
 
-'''ASYL = Average Num of Syllables'''
-# Calculate the average number of syllables per word in the paragraph.
-total_syllables = 0
-for word in words:
-    total_syllables += find_num_syllables(word)
+    '''ASYL = Average Num of Syllables'''
+    # Calculate the average number of syllables per word in the paragraph.
+    total_syllables = 0
+    for word in words:
+        total_syllables += find_num_syllables(word)
 
-ASYL = total_syllables/len(words)
+    ASYL = total_syllables/len(words)
 
-'''GFRI = Gunning Fog Readability Index'''
-# Calculate the Gunning Fog Readability Index of the paragraph.
-GFRI = 0.4 * (ASL + PHW)
+    '''GFRI = Gunning Fog Readability Index'''
+    # Calculate the Gunning Fog Readability Index of the paragraph.
+    GFRI = 0.4 * (ASL + PHW)
 
-'''FKRI = Flesch-Kincaid Readability Index'''
-# Calculate the Flesch-Kincaid Readability Index of the paragraph.
-FKRI = (206.835 - 1.015 * ASL) - (86.4 * ASYL)
+    '''FKRI = Flesch-Kincaid Readability Index'''
+    # Calculate the Flesch-Kincaid Readability Index of the paragraph.
+    FKRI = (206.835 - 1.015 * ASL) - (86.4 * ASYL)
 
-# Print the results
-print('Here are the hard words in this paragraph:')
-print(hard_words)
-print('Statistics: ASL:{:.2f} PHW:{:.2f}% ASYL:{:.2f}'.format(ASL, PHW, ASYL))
-print('Readbility index (GFRI): {:.2f}'.format(GFRI))
-print('Readbility index (FKRI): {:.2f}'.format(FKRI))
+    # Print the results
+    print('Here are the hard words in this paragraph:')
+    print(hard_words)
+    print('Statistics: ASL:{:.2f} PHW:{:.2f}% ASYL:{:.2f}'.format(ASL, PHW, ASYL))
+    print('Readbility index (GFRI): {:.2f}'.format(GFRI))
+    print('Readbility index (FKRI): {:.2f}'.format(FKRI))
---
+++ Homework/Homework3/hw3_part2.py
@@ -0,0 +1,20 @@
+"""Pikachu in the Wild! This program simulates a pokemon game, where the user directs pikachu
+to move accross a 150x150 grid. The user can direct pikachu to move up, down, left, or right.
+Pikachu will move in the direction specified by the user until it reaches the edge of the grid.
+Users can name their pikachu and can also set the number of turns it takes to see a wild pokemon.
+The user can also choose which pokemon type they want to battle.
+
+Author: Jimmy Chen
+Version: 1
+"""
+
+'''This function takes the current coordinates of pikachu and the direction to move, as well as the steps to move.
+It returns the new coordinates of pikachu after moving in the specified direction. The return value is a tuple,
+and must be within the bounds of the grid. Any invalid direction will be ignored'''
+def move_pokemon(coords, direction, steps):
+    # Your code starts here
+    pass
+
+if __name__ == "__main__":
+    # Your code starts here
+    pass
