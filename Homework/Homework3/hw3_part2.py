601ac02ee4f6019ce055fb2bfd4a5131c380a420
c-jimmy1 <74884278+c-jimmy1@users.noreply.github.com>
2025-02-16 23:45:12 -0500

    final bug fix, fully working pt 2, add comments

--- Homework/Homework3/hw3_part2.py
+++ Homework/Homework3/hw3_part2.py
@@ -8,7 +8,14 @@ Author: Jimmy Chen
 Version: 1
 """
 
+
+'''This function simulates the pokemon game. It takes the number of turns, the name of the pikachu, and the battle frequency
+as inputs. The function will prompt the user to input the direction to move the pikachu, and the type of pokemon to battle.
+The function will then move the pikachu in the specified direction, and will battle the pokemon if the turn is a multiple of the
+battle frequency. The function will keep track of the record of battles, and will print the final coordinates of pikachu, as well as
+the battle record.'''
 def simulation_loop(turns, name, battle_freq):
+    # initialize the turn number, and the starting coordinates of pikachu
     curr_turn = 0
     curr_coords = (75, 75)
     print("Starting simulation, turn {} {} at {}".format(curr_turn, name, curr_coords))
@@ -16,26 +23,35 @@ def simulation_loop(turns, name, battle_freq):
 
     record = []
     
+    # loop through the number of turns specified
     while curr_turn <= turns:
-        curr_direction = input("What direction does Piki walk? => ").strip()
+        # get the direction to move pikachu
+        curr_direction = input("What direction does {} walk? => ".format(name)).strip()
         print(curr_direction)
+
+        # move pikachu in the specified direction
         curr_coords = move_pokemon(curr_coords, curr_direction, 5)
+
+        # check if pikachu encounters a pokemon
         if curr_turn % battle_freq == 0:
             print("Turn {}, {} at {}".format(curr_turn, name, curr_coords))
-            opp_type = input("What type of Pokemon do you meet (W)ater, (G)round? => ").strip()
+            opp_type = input("What type of pokemon do you meet (W)ater, (G)round? => ").strip()
             print(opp_type)
+
+            # battle the pokemon
             if opp_type.upper() == "W":
                 curr_coords = move_pokemon(curr_coords, curr_direction, 1)
                 print("{} wins and moves to {}".format(name, curr_coords))
                 record.append('Win')
             elif opp_type.upper() == "G":
-                if curr_direction.upper() == "U":
+                # if pikachu loses the battle, it will run away in the opposite direction
+                if curr_direction.upper() == "S":
                     curr_coords = move_pokemon(curr_coords, "N", 10)
-                elif curr_direction.upper() == "D":
+                elif curr_direction.upper() == "N":
                     curr_coords = move_pokemon(curr_coords, "S", 10)
-                elif curr_direction.upper() == "L":
+                elif curr_direction.upper() == "E":
                     curr_coords = move_pokemon(curr_coords, "W", 10)
-                elif curr_direction.upper() == "R":
+                elif curr_direction.upper() == "W":
                     curr_coords = move_pokemon(curr_coords, "E", 10)
                 print("{} runs away to {}".format(name, curr_coords))
                 record.append('Lose')
@@ -51,6 +67,8 @@ It returns the new coordinates of pikachu after moving in the specified directio
 and must be within the bounds of the grid. Any invalid direction will be ignored'''
 def move_pokemon(coords, direction, steps):
     x, y = coords
+
+    # move pikachu in the specified direction
     if direction.upper() == "N":
         x -= steps
     elif direction.upper() == "S":
@@ -59,6 +77,16 @@ def move_pokemon(coords, direction, steps):
         y -= steps
     elif direction.upper() == "E":
         y += steps
+
+    # check if the new coordinates are within the bounds of the grid
+    if x < 0:
+        x = 0
+    elif x > 150:
+        x = 150
+    if y < 0:
+        y = 0
+    elif y > 150:
+        y = 150
     return (x, y)
 
 if __name__ == "__main__":
