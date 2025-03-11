ab75bbad40b04caedf0b512bf773a654c80d7880
Jimmy Chen <jc219570512@gmail.com>
2025-03-11 00:20:27 -0400

    init hw 5

---
+++ Homework/Homework5/hw5_part1.py
@@ -0,0 +1,9 @@
+"""
+Part 1 of our program asks us to mimic a hill-climing search. We will take a list of lists that holds heights.
+Our goals for part 1 is to find neighbors given a start locations, be able to print the grid, and also see if a path is valid.
+Author: Jimmy Chen
+Version: 1
+"""
+
+import hw5_util
+
